30213ad628b216cb07629787c1a91903bcf3b3e4
Emmanuel <zacmac807@gmail.com>
2024-06-01 11:16:23 +0300

    complete task

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,34 +1,3 @@
-# class Pet:
-#     all = []
-
-#     PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
-#     def __init__(self,name,pet_type,owner=None) -> None:
-#         self.name = name
-#         self.owner = owner
-#         if not pet_type in Pet.PET_TYPES:
-#             raise Exception("Pet is not in the list")
-#         self.pet_type =pet_type
-#         Pet.all.append(self)
-
-
-# class Owner:
-#     def __init__(self,name) -> None:
-#         self.name = name
-#         # self.pets = []
-
-
-#     def pets(self):
-#         return [pet for pet in Pet.all if pet.owner.name == self.name]
-
-#     def add_pet(self,pet):
-#         if not isinstance(pet ,Pet):
-#             raise TypeError("The value must be an instance of the pet class ")
-#         pet.owner = self
-
-#     def get_sorted_pets(self):
-#         return sorted(self.pets(),key = lambda pet:pet.owner)
-
-
 class Pet:
     all = []
 
@@ -56,4 +25,4 @@ class Owner:
         pet.owner = self
 
     def get_sorted_pets(self):
-        return sorted(self.pets(), key=lambda pet: pet.name)
+        return sorted(self.pets(), key = lambda pet: pet.name)
